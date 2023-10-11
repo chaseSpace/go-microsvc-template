2ff3ea37ba1892164009f71f00852cfe372533d1
lei <random2035@qq.com>
2023-10-12 00:36:58 +0800

    update user model

--- docs/sql/20230817_init.sql
+++ docs/sql/20230817_init.sql
@@ -4,18 +4,15 @@ create database biz_log;
 drop table if exists biz.user;
 create table biz.user
 (
-    id          int primary key auto_increment,
-    uid         int              not null,
-    ext_uid     int              not null,
-    nickname    varchar(20)      not null,
-    age         tinyint unsigned not null,
-    sex         tinyint unsigned not null,
-    account     varchar(30)      not null,
-    password    varchar(40)      not null comment 'encrypted with salt in SHA1',
-    passwd_salt varchar(10)      not null,
-    created_at  datetime         not null default current_timestamp,
-    updated_at  datetime         not null default current_timestamp on update current_timestamp,
-    key idx_uid (uid),
-    key idx_extuid (ext_uid),
+    id            int primary key auto_increment,
+    uid           int              not null,
+    nickname      varchar(20)      not null,
+    birthday      date             not null,
+    sex           tinyint unsigned not null comment '0-未知 1男2女',
+    password      varchar(40)      not null,
+    password_salt varchar(10)      not null,
+    created_at    datetime         not null default current_timestamp,
+    updated_at    datetime         not null default current_timestamp on update current_timestamp,
+    unique key idx_uid (uid),
     key idx_ct (created_at)
 )
