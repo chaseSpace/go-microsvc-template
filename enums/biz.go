7d86caa17e7168dcf512081997580fd4ff8857ba
lei <random2035@qq.com>
2023-10-12 00:37:49 +0800

    1

--- enums/biz.go
+++ enums/biz.go
@@ -12,6 +12,6 @@ func (s Sex) Int32() int32 {
 	return int32(s)
 }
 
-func (s Sex) IsValid() bool {
-	return s == SexMale || s == SexFemale
+func (s Sex) IsInvalid() bool {
+	return s != SexMale && s != SexFemale
 }
