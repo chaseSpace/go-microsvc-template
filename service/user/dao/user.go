a4c96d706ea2530e6623afc3e1dbec094aa973b0
lei <random2035@qq.com>
2023-10-12 00:42:32 +0800

    add GetMaxUid, IsUidExists

--- service/user/dao/user.go
+++ service/user/dao/user.go
@@ -1,9 +1,28 @@
 package dao
 
 import (
+	"gorm.io/gorm"
 	"microsvc/infra/orm"
+	"microsvc/pkg/xerr"
 	"microsvc/proto/model/user"
 )
+
+func GetMaxUid() (int64, error) {
+	row := new(user.User)
+	err := user.Q.Order("uid desc").Take(row).Error
+	return row.Uid, err
+}
+
+func IsUidExists(uid uint64) (bool, error) {
+	if uid < 1 {
+		return false, xerr.ErrParams.AppendMsg("invalid uid on insert")
+	}
+	exec := user.Q.Take(&user.User{}, "uid=?", uid)
+	if exec.Error != nil && exec.Error != gorm.ErrRecordNotFound {
+		return false, exec.Error
+	}
+	return exec.RowsAffected > 0, nil
+}
 
 func GetUser(uid ...int64) (list []*user.User, row user.User, err error) {
 	if len(uid) == 1 {
