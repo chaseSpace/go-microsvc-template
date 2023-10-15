e303d23d7a04a5c35627144ed582fab056d67524
lei <random2035@qq.com>
2023-10-15 12:49:36 +0800

    optimize GetUser()

--- service/user/cache/user.go
+++ service/user/cache/user.go
@@ -4,25 +4,28 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"microsvc/bizcomm/commuser"
+	"github.com/samber/lo"
 	"microsvc/proto/model/user"
 	"microsvc/service/user/dao"
 )
 
-func GetUser(uid ...int64) (umap map[int64]*user.User, err error) {
+type UserMap map[int64]*user.User
+
+func GetUser(uid ...int64) (umap UserMap, err error) {
 	var keys []string
 	for _, u := range uid {
-		keys = append(keys, fmt.Sprintf(UserCacheKey, u))
+		keys = append(keys, fmt.Sprintf(UserInfoCacheKey, u))
 	}
 	reply := user.R.MGet(context.Background(), keys...)
 	if reply.Err() != nil {
-		return nil, err
+		return nil, reply.Err()
 	}
-	umap = make(map[int64]*user.User, len(uid))
-	var queryDBUids []int64
+	umap = make(UserMap, len(uid))
+
+	var cacheMissUids []int64
 	for i, v := range reply.Val() {
 		if v == nil {
-			queryDBUids = append(queryDBUids, uid[i])
+			cacheMissUids = append(cacheMissUids, uid[i])
 			continue
 		}
 		u := new(user.User)
@@ -30,12 +33,14 @@ func GetUser(uid ...int64) (umap map[int64]*user.User, err error) {
 		umap[u.Uid] = u
 	}
 
-	if len(queryDBUids) > 0 {
-		list, _, err := dao.GetUser(queryDBUids...)
+	if len(cacheMissUids) > 0 {
+		list, _, err := dao.GetUser(cacheMissUids...)
 		if err != nil {
 			return nil, err
 		}
-		commuser.MergeUserListToMap(umap, list)
+		lo.ForEach(list, func(item *user.User, index int) {
+			umap[item.Uid] = item
+		})
 	}
 	return
 }
