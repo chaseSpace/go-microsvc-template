1b4cd579a60ecb115aabe2ac8bcb33ba0201108d
Leigg <random2035@qq.com>
2023-12-02 16:02:52 +0800

    update

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-# 此脚本仅需要支持在linux环境执行
--- build_pb.sh
+++ build_pb.sh
@@ -5,15 +5,25 @@ set -e
 # - mac环境请在根目录下新建 tool_mac目录（不要提交到git），并自行下载tool/目录下的二进制文件，脚本会自动调用
 
 _pb_dir=$1 # 指定生成的pb子目录，比如user 对应 microsvc/svc/user
-_bin_path='./tool/protoc_v24'
-if [[ $(arch) = 'arm64' ]]; then
-  echo '*** within mac env ***'
-  _bin_path='./tool_mac/protoc_v24'
+_protoc_path=""
+
+if [[ $(uname) == "Linux" ]]; then
+    echo "Running on Linux"
+    _protoc_path='./tool/protoc_v24'
+elif [[ $(uname) == "Darwin" ]]; then
+    echo "Running on macOS"
+    _protoc_path='./tool_mac/protoc_v24'
+elif [[ $(uname) == *MINGW* ]]; then
+    echo "Running on Windows"
+    _protoc_path='./tool_win/protoc_v24'
+else
+    echo "Unknown OS"
+    exit 1
 fi
 
-chmod +x $_bin_path/*
+chmod +x $_protoc_path/*
 
-PATH=$PATH:$_bin_path
+PATH=$PATH:$_protoc_path
 
 OUTPUT_DIR="./protocol/"
 mkdir -p $OUTPUT_DIR
@@ -27,7 +37,7 @@ clear)
     echo "regenerate proto/svc/$_pb_dir/..."
     rm -rf ./protocol/$_pb_dir/*
 
-    $_bin_path/protoc -I ./proto/ -I ./proto/include/ \
+    $_protoc_path/protoc -I ./proto/ -I ./proto/include/ \
       --go_out=$OUTPUT_DIR \
       --go_opt paths=source_relative \
       --go-grpc_out=$OUTPUT_DIR \
@@ -43,7 +53,7 @@ clear)
     echo "regenerate all proto files..."
     rm -rf ./protocol/*
 
-    $_bin_path/protoc -I ./proto/ -I ./proto/include/ \
+    $_protoc_path/protoc -I ./proto/ -I ./proto/include/ \
       --go_out=$OUTPUT_DIR \
       --go_opt paths=source_relative \
       --go-grpc_out=$OUTPUT_DIR \
