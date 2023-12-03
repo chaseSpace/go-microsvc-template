d14c800df14efa2f4f7d9ca8493d9d646626bf73
Leigg <random2035@qq.com>
2023-12-03 15:25:10 +0800

    +注释

--- enums/svc/svc.go
+++ enums/svc/svc.go
@@ -2,11 +2,15 @@ package svc
 
 type Svc string
 
+// Name 请不要随意修改这个方法的逻辑，因为微服务客户端证书中的subjectAltName包含了这个Name
+// 一旦修改，客户端证书需要重新申请，否则RPC请求将会失败：tls: bad certificate
+// （一种不安全的做法是取消RPC的双向证书验证）
+// 参阅：generate_cert_for_svc.md
 func (s Svc) Name() string {
 	if s == "" {
 		return "unknown-svc"
 	}
-	return "go-" + string(s)
+	return string(s)
 }
 
 const (
